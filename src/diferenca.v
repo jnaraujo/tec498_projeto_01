929e74050a449d657ba6957759c1df8be92e8877
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-13 20:17:02 -0300

    ✨ feat: add complement of 2 on diferenca

--- src/diferenca.v
+++ src/diferenca.v
@@ -13,12 +13,16 @@ module diferenca(S, sinal, A, B);
 	comparador comp0(A, B, maior, menor, igual); // verifica A em relação a B: se ele é maior, menor ou igual
 	
 	subtrator sub0(resSub, sub_bout, A, B); // subtrai B de A
-	
-	xor xor0(resXor[0], resSub[0], menor);
+
+  // complemento de 2 de B
+
+	xor xor0(resXor[0], resSub[0], menor); 
 	xor xor1(resXor[1], resSub[1], menor);
-	xor xor2(resXor[2], resSub[2], menor);
+	xor xor2(resXor[2], resSub[2], menor); 
 	xor xor3(resXor[3], resSub[3], menor);
 	
+	adder(S, resXor, menor, 0); // se A for menor que B, então soma 1 bit ao resultado
+
   // define o sinal da operacao
 	
 	assign sinal = menor; // 1 = negativo
