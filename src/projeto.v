0302afad790335bfe706237d6faf7ab946ef8785
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-13 20:40:29 -0300

    ✨ feat: Improved projeto.v logical code

--- src/projeto.v
+++ src/projeto.v
@@ -11,10 +11,14 @@ module projeto(senha, tentativa, led0, led1, led2, diferenca);
 	
 	wire sinal;
 	wire maior, menor, igual;
+	wire not_igual;
 	wire bit3, bit2;
 	wire notBit3, notBit2;
+	wire nb3_and_nb2;
 	
 	comparador comp0(senha, tentativa, maior, menor, igual); // verifica A em relação a B: se ele é maior, menor ou igual
+	
+	not not0(not_igual, igual);
 	
 	// sinal: 1 = negativo;
 	diferenca diff0 (diferenca, sinal, senha, tentativa);
@@ -29,7 +33,8 @@ module projeto(senha, tentativa, led0, led1, led2, diferenca);
 	not(notBit3, bit3);
 	not(notBit2, bit2);
 	
-	and and3 (led1, notBit3, notBit2); // se algum dos dois for positivo, a diferença entre as duas senhas excede 3 unidades
+	and and3 (nb3_and_nb2, notBit3, notBit2); // se algum dos dois for positivo, a diferença entre as duas senhas excede 3 unidades
+	and and4 (led1, nb3_and_nb2, not_igual); // se a senha estiver correta, led1 nao eh acionado
 	or or0 (led2, bit3, bit2); // se a diferença for maior que 3 unidades, o cofre nao eh aberto
 	
 endmodule 
