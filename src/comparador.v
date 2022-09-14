8462303b551e7b12eb73eca8447da0e8397a25e8
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-14 10:08:19 -0300

    🔥 chore: Change assign to logical ports on comparador.v
    Co-authored-by: Yasmin <yasmincsme@gmail.com>

--- src/comparador.v
+++ src/comparador.v
@@ -5,6 +5,7 @@ module comparador(A, B, maior, menor, igual);
 	output igual, maior, menor;
 	
 	wire [3:0] aux_igual, aux_menor, aux_maior;
+	wire w0, w1, w2, w3;
 	
 	comparador1bit bit3 (A[3], B[3], 1,            aux_maior[3], aux_menor[3], aux_igual[3]); // compara o bit 3 do A com o do B
 	
@@ -12,10 +13,17 @@ module comparador(A, B, maior, menor, igual);
 	
 	comparador1bit bit1 (A[1], B[1], aux_igual[2], aux_maior[1], aux_menor[1], aux_igual[1]); // compara o bit 1 do A com o do B
 	
-	comparador1bit bit0 (A[0], B[0], aux_igual[1], aux_maior[0], aux_menor[0], aux_igual[0]); // compara o bit 0 do A com o do B
+	comparador1bit bit0 (A[0], B[0], aux_igual[1], aux_maior[0], aux_menor[0], igual); // compara o bit 0 do A com o do B
 	
-	assign maior = | aux_maior; // OR em todos os bits do aux_maior
-	assign menor = | aux_menor; // OR em todos os bits do aux_menor
-	assign igual = aux_igual[0]; // define igual
+	// OR em todos os bits do aux_maior
+	
+	or or0(w0, aux_maior[0], aux_maior[1]);
+	or or1(w1, aux_maior[2], aux_maior[3]);
+	or or2(maior, w0, w1);
+	
+	// OR em todos os bits do aux_menor
+	or or3(w2, aux_menor[0], aux_menor[1]);
+	or or4(w3, aux_menor[2], aux_menor[3]);
+	or or5(menor, w2, w3);
 	
 endmodule 
