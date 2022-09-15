4e403744f120624f3abfb73b9c9f0d12212b35f9
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-15 13:13:06 -0300

    ✨ feat: Add display to projeto

--- src/projeto.v
+++ src/projeto.v
@@ -1,4 +1,4 @@
-module projeto(senha, tentativa, led0, led1, led2, display);
+module projeto(senha, tentativa, led0, led1, led2, segD, A, B, D, E, F, G, DP);
 	// led0: quando o cofre é aberto
 	// led1: quando a senha é maior ou menor em ate 3 unidades
 	// led2: se a senha estiver errada
@@ -9,7 +9,7 @@ module projeto(senha, tentativa, led0, led1, led2, display);
 	
 	// Define as saídas do circuito
 	output led0, led1, led2;
-	output [7:0] display;
+	output segD, A, B, D, E, F, G, DP;
 	
 	// Define os fios
 	wire subBOut; // sinal da operação; 1 == negativo
@@ -30,6 +30,16 @@ module projeto(senha, tentativa, led0, led1, led2, display);
 		.errada(led2)
 	);
 	
-	
-	
+	display display0 (.diff(diferenca),
+							.sinal(subBOut),
+							.segD(segD),
+							.A(A),
+							.B(B),
+							.C(C),
+							.D(D),
+							.E(E),
+							.F(F),
+							.G(G),
+							.DP(DP)
+	);
 endmodule 
