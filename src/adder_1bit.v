f6a487a682a757430dd8ef4653fb1ca20f4daf4a
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-15 18:48:39 -0300

    🔥 chore: Improved adder_1bit code

--- src/adder_1bit.v
+++ src/adder_1bit.v
@@ -1,11 +1,17 @@
 module adder_1bit( S, Cout, A, B, Cin);
+	// define as entradas
    input A, B, Cin;
+	
+	// define as saidas
    output S, Cout;
-   wire T1, T2, T3;
+	
+	// define os fios
+   wire A_XOR_B, A_AND_B;
+	wire w0;
  
-   xor xor0 (T1, A, B);
-   and and0(T2, A, B);
-   and and1(T3, T1, Cin);
-   or or0(Cout, T2, T3);
-   xor xor1 (S, T1, Cin);
+   xor xor0 (A_XOR_B, A, B);
+   and and0(A_AND_B, A, B);
+   and and1(w0, A_XOR_B, Cin);
+   or or0(Cout, A_AND_B, w0);
+   xor xor1 (S, A_XOR_B, Cin);
 endmodule 
