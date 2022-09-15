6d062b7ea8144f37badc4e7b87385e241b62dff2
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-15 10:51:50 -0300

    🔥 chore: change output diferenca to display

--- src/projeto.v
+++ src/projeto.v
@@ -1,4 +1,4 @@
-module projeto(senha, tentativa, led0, led1, led2, diferenca);
+module projeto(senha, tentativa, led0, led1, led2, display);
 	// led0: quando o cofre é aberto
 	// led1: quando a senha é maior ou menor em ate 3 unidades
 	// led2: se a senha estiver errada
@@ -9,7 +9,7 @@ module projeto(senha, tentativa, led0, led1, led2, diferenca);
 	
 	// Define as saídas do circuito
 	output led0, led1, led2;
-	output [3:0] diferenca;
+	output [7:0] display;
 	
 	// Define os fios
 	wire subBOut; // sinal da operação; 1 == negativo
